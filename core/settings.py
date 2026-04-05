80f8276989c58afaafcd3fbc4fdcf1729f43861c
edberto <edybanera@gmail.com>
2026-04-05 15:07:25 -0400

    update

--- core/settings.py
+++ core/settings.py
@@ -71,6 +71,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', 'secret'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        }
     }
 }
 
