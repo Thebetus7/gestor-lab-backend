ceef64c1f35334fde06068c3446175bef06f9f41
edberto <edybanera@gmail.com>
2026-06-25 12:04:19 -0400

    ok1

--- core/settings.py
+++ core/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
